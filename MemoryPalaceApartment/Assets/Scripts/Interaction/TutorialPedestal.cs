d98dec27e9edba24265d1530345071d3e7ea6dcb
LKrueger <linda.kruger.2017@mumail.ie>
2020-02-26 17:25:21 +0000

    objects less bouncy, no strange throwing behaviour, tutorial partially working

---
+++ MemoryPalaceApartment/Assets/Scripts/Interaction/TutorialPedestal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPedestal : MonoBehaviour
+{
+    //public bool object1 = false;
+    public GameObject exit = null;
+    public string myTag = null;
+    public int booleanAssigned = 0;
+
+    private void Awake()
+    {
+        //object1 = exit.GetComponent<tutorialRoomExit>().object1;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collider triggered");
+        if (other.CompareTag(myTag))
+        {
+            Debug.Log("compare tag " + myTag + " ran");
+            if (booleanAssigned == 1)
+                exit.GetComponent<tutorialRoomExit>().object1 = true;
+            else if (booleanAssigned == 2)
+                exit.GetComponent<tutorialRoomExit>().object2 = true;
+            else if (booleanAssigned == 3)
+                exit.GetComponent<tutorialRoomExit>().object3 = true;
+            else
+                Debug.Log("Error finding correct boolean");
+            exit.GetComponent<tutorialRoomExit>().deactivateDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(myTag))
+        {
+            if (booleanAssigned == 1)
+                exit.GetComponent<tutorialRoomExit>().object1 = false;
+            else if (booleanAssigned == 2)
+                exit.GetComponent<tutorialRoomExit>().object2 = false;
+            else if (booleanAssigned == 3)
+                exit.GetComponent<tutorialRoomExit>().object3 = false;
+            else
+                Debug.Log("Error finding correct boolean");
+        }
+    }
+}
---
+++ MemoryPalaceApartment/Assets/Scripts/Interaction/tutorialRoomExit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialRoomExit : MonoBehaviour
+{
+    public bool object1 = false;
+    public bool object2 = false;
+    public bool object3 = false;
+    // Start is called before the first frame update
+
+
+    public void deactivateDoor()
+    {
+        if (object1 == object2 == object3 == true)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
